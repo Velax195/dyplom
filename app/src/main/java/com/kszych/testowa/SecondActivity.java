4dad1636d40e0296e876c32e94125ab99a9c2683
Krzysztof Szych <222852@student.pwr.edu.pl>
2017-10-08 22:55:05 +0200

    increment, decrement on hold

--- app/src/main/java/com/kszych/testowa/SecondActivity.java
+++ app/src/main/java/com/kszych/testowa/SecondActivity.java
@@ -1,8 +1,10 @@
 package com.kszych.testowa;
 
+import android.os.Handler;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,6 +20,7 @@ public class SecondActivity extends AppCompatActivity {
 
     private Button btIncrement;
     private Button btDecrement;
+    private Handler hdHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,54 +36,77 @@ public class SecondActivity extends AppCompatActivity {
         }
 
         tvValue.setText(getString(R.string.single_digit, iCurrentVal));
-
         btIncrement = (Button) findViewById(R.id.btValIncrement);
-
-        btIncrement.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (iCurrentVal<100)
-                {
-                    iCurrentVal++;
-                    tvValue.setText(getString(R.string.single_digit, iCurrentVal));
-                }
-                else
-                {
-                    Toast.makeText(SecondActivity.this, "Upper limit reached" , Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
         btDecrement = (Button) findViewById(R.id.btValDecrement);
 
-        btDecrement.setOnClickListener(new View.OnClickListener() {
+        btIncrement.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View view) {
-                if (iCurrentVal>0)
-                {
-                    iCurrentVal--;
-                    tvValue.setText(getString(R.string.single_digit, iCurrentVal));
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch(motionEvent.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        if(hdHandler!=null) return true;
+                        hdHandler=new Handler();
+                        hdHandler.postDelayed(rAction, 500);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        if(hdHandler==null) return true;
+                        hdHandler.removeCallbacks(rAction);
+                        hdHandler=null;
+                        break;
                 }
-                else
-                {
-                    Toast.makeText(SecondActivity.this, "Lower limit reached" , Toast.LENGTH_SHORT).show();
-                }
+                return false;
             }
-        });
-        btDecrement.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-
-                    if (iCurrentVal > 0) {
-                        iCurrentVal=0;
+            Runnable rAction = new Runnable() {
+                @Override
+                public void run() {
+                    if (iCurrentVal<100)
+                    {
+                        iCurrentVal++;
                         tvValue.setText(getString(R.string.single_digit, iCurrentVal));
-                    } else {
-                        Toast.makeText(SecondActivity.this, "Lower limit reached", Toast.LENGTH_SHORT).show();
                     }
+                    else
+                    {
+                        Toast.makeText(SecondActivity.this, "Upper limit reached" , Toast.LENGTH_SHORT).show();
+                    }
+                    hdHandler.postDelayed(this, 500);
+                }
+    };
+});
 
-                return true;
+        btDecrement.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch(motionEvent.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        if(hdHandler!=null) return true;
+                        hdHandler=new Handler();
+                        hdHandler.postDelayed(rAction, 500);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        if(hdHandler==null) return true;
+                        hdHandler.removeCallbacks(rAction);
+                        hdHandler=null;
+                        break;
+                }
+                return false;
             }
+            Runnable rAction = new Runnable() {
+                @Override
+                public void run() {
+                    if (iCurrentVal>0)
+                    {
+                        iCurrentVal--;
+                        tvValue.setText(getString(R.string.single_digit, iCurrentVal));
+                    }
+                    else
+                    {
+                        Toast.makeText(SecondActivity.this, "Upper limit reached" , Toast.LENGTH_SHORT).show();
+                    }
+                    hdHandler.postDelayed(this, 500);
+                }
+            };
         });
+
 
         }
     }
