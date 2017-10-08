1458cd809e2131c4247e48300c10c01b2895759f
Krzysztof Szych <222852@student.pwr.edu.pl>
2017-10-08 16:40:53 +0200

    test changes 2

--- app/src/main/java/com/kszych/testowa/MainActivity.java
+++ app/src/main/java/com/kszych/testowa/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "some test");
+        // naprawilem blad
     }
 }
