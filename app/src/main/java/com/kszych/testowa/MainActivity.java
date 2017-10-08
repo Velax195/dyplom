cdd779553a1cc43d01503e12b6ee6ba622294cba
Krzysztof Szych <222852@student.pwr.edu.pl>
2017-10-08 16:32:15 +0200

    test changes

--- app/src/main/java/com/kszych/testowa/MainActivity.java
+++ app/src/main/java/com/kszych/testowa/MainActivity.java
@@ -2,12 +2,16 @@ package com.kszych.testowa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "some test");
     }
 }
