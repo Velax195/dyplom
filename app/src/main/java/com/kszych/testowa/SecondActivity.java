97cffa02a87c90f475eb97e451754e125e890262
Krzysztof Szych <222852@student.pwr.edu.pl>
2017-10-08 22:06:03 +0200

    increment, decrement, zero on long decrement

--- app/src/main/java/com/kszych/testowa/SecondActivity.java
+++ app/src/main/java/com/kszych/testowa/SecondActivity.java
@@ -1,8 +1,12 @@
 package com.kszych.testowa;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -12,6 +16,9 @@ public class SecondActivity extends AppCompatActivity {
 
     private int iCurrentVal;
 
+    private Button btIncrement;
+    private Button btDecrement;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,14 +26,62 @@ public class SecondActivity extends AppCompatActivity {
 
         tvValue = (TextView) findViewById(R.id.tvSecondContent);
 
-        if(getIntent().getExtras() != null) {
+        if (getIntent().getExtras() != null) {
             iCurrentVal = getIntent().getExtras().getInt(MainActivity.CROSS_ACTIVITY_MESSAGE, 0);
-        }
-        else {
+        } else {
             iCurrentVal = 0;
         }
 
         tvValue.setText(getString(R.string.single_digit, iCurrentVal));
 
+        btIncrement = (Button) findViewById(R.id.btValIncrement);
+
+        btIncrement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (iCurrentVal<100)
+                {
+                    iCurrentVal++;
+                    tvValue.setText(getString(R.string.single_digit, iCurrentVal));
+                }
+                else
+                {
+                    Toast.makeText(SecondActivity.this, "Upper limit reached" , Toast.LENGTH_SHORT).show();
+                }
             }
-}
+        });
+
+        btDecrement = (Button) findViewById(R.id.btValDecrement);
+
+        btDecrement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (iCurrentVal>0)
+                {
+                    iCurrentVal--;
+                    tvValue.setText(getString(R.string.single_digit, iCurrentVal));
+                }
+                else
+                {
+                    Toast.makeText(SecondActivity.this, "Lower limit reached" , Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btDecrement.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+
+                    if (iCurrentVal > 0) {
+                        iCurrentVal=0;
+                        tvValue.setText(getString(R.string.single_digit, iCurrentVal));
+                    } else {
+                        Toast.makeText(SecondActivity.this, "Lower limit reached", Toast.LENGTH_SHORT).show();
+                    }
+
+                return true;
+            }
+        });
+
+        }
+    }
+
