f7f2982b74e4f3033eaa1b7323a066dd76ea7e27
Krzysztof Szych <222852@student.pwr.edu.pl>
2017-10-08 17:44:48 +0200

    first screen: the beginning

--- app/src/main/java/com/kszych/testowa/MainActivity.java
+++ app/src/main/java/com/kszych/testowa/MainActivity.java
@@ -3,8 +3,15 @@ package com.kszych.testowa;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etInput;
+    private Button btGoToNext;
 
     private static String TAG = "MainActivity";
 
@@ -12,7 +19,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "some test");
-        // naprawilem blad
+
+        etInput = (EditText) findViewById(R.id.etOnlyNumbers);
+        btGoToNext = (Button) findViewById(R.id.btGoToSecond);
+
+        btGoToNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "temporary message", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
